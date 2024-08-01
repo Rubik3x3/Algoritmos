2a849698db33ebc73bda4cdac5424993d67c1be4
Rubik3x3 <franco.f4629@gmail.com>
2024-08-01 16:26:52 -0300

    Ejercicio 1

---
+++ ejercicios/1.py
@@ -0,0 +1,18 @@
+"""
+Solicitar al usuario que ingrese su dirección email. 
+Imprimir un mensaje indicando si la dirección es válida o no, valiéndose de una función para decidirlo. 
+Una dirección se considerará válida si contiene el símbolo "@".
+"""
+
+def validarEmail(email):
+    for i in email:
+        if i == "@":
+            return True
+    return False
+
+email_ingresado = str(input("Ingrese su email: "))
+
+if(validarEmail(email_ingresado)):
+    print(f'El email {email_ingresado} es válido.')
+else:
+    print(f'El email {email_ingresado} NO es válido.')
