1e73d5ddb35bbdcf4e20ce4405dda492c9f2948e
Franco Salvador Talarico <84205093+Rubik3x3@users.noreply.github.com>
2024-08-02 14:25:56 -0300

    14

--- ejercicios/ejercicio_14.py
+++ ejercicios/ejercicio_14.py
@@ -1,15 +1,20 @@
 def titulo(string):
     siguiente_mayuscula = True
+    palabra_con_mayuscula = False
     string_modificado = ""
     for i in string:
-        if(i != ' '):
+        if(i != ' ') and palabra_con_mayuscula == False:
             siguiente_mayuscula = True
         if(siguiente_mayuscula):
             string_modificado += i.upper()
             siguiente_mayuscula = False
+            palabra_con_mayuscula = True
         else:
             string_modificado += i.lower()
+        if(i == ' '):
+            palabra_con_mayuscula = False
     return string_modificado
-texto = "hola cOmO eStAS ABC"
+
+texto = str(input("Ingrese un texto: "))
 
 print(titulo(texto))
